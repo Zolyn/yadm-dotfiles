943411a1b5a58b0f260ef0e0953bc8b96d162f96
Zolyn <zorin1477@gmail.com>
2022-05-15 10:54:35 +0800

    update

--- MyFiles/scripts/init.sh
+++ MyFiles/scripts/init.sh
@@ -2,7 +2,9 @@
 dir=$(dirname "$0")
 export PATH=$PATH:/home/zorin/.local/bin
 
-nohup bash ~/MyFiles/projects/i3-video-wallpaper/setup.sh -p ~/视频/雪黎.mp4 -a -n -w -b -f max -g 64x64 > ~/log/vwp.out &
+nohup bash ~/MyFiles/projects/i3-video-wallpaper/setup.sh -a -n -w -b -f max -g 42x40 -p ~/视频/伊蕾娜_咖啡.mp4 > ~/log/vwp.out
+# nohup bash ~/MyFiles/projects/i3-video-wallpaper/setup.sh -a -n -w -b -p ~/视频/魔女之旅_贴贴.mp4 > ~/log/vwp.out &
+# nohup bash ~/MyFiles/projects/i3-video-wallpaper/setup.sh -p ~/视频/雪黎.mp4 -a -n -w -b -f max -g 64x64 > ~/log/vwp.out &
 # nohup clash > ~/log/clash.out &
 nohup deadd-notification-center > ~/log/notification.out &
 # nohup bash "$dir/qq_notify_1.sh" > ~/log/qq_notify_1.out &
