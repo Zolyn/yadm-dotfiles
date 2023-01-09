194d6d50c1114c26c0ea2c3a68a13d62ccceb125
Yumeoto Zorin <zorin1477@gmail.com>
2023-01-09 19:00:41 +0800

    chore: update

--- MyFiles/scripts/init.sh
+++ MyFiles/scripts/init.sh
@@ -10,13 +10,13 @@ xrandr --output DisplayPort-2 --primary --right-of HDMI-A-0
 nohup imwheel &
 
 # Video Wallpaper
-nohup bash ~/MyFiles/projects/i3-video-wallpaper/setup.sh -nwb ~/视频/yang_00000_1.mp4,32x32 ~/视频/伊蕾娜_咖啡.mp4 > ~/log/vwp.out
+nohup bash ~/MyFiles/projects/i3-video-wallpaper/setup.sh -nwb ~/视频/yang_00000_1.mp4,32x32 ~/视频/伊蕾娜_咖啡.mp4 > ~/log/vwp.out &
 # nohup bash ~/MyFiles/projects/i3-video-wallpaper/setup.sh -a -n -w -b -f max -g 42x40 -p ~/视频/伊蕾娜_咖啡.mp4 > ~/log/vwp.out
 # nohup bash ~/MyFiles/projects/i3-video-wallpaper/setup.sh -a -n -w -b -p ~/视频/魔女之旅_贴贴.mp4 > ~/log/vwp.out &
 # nohup bash ~/MyFiles/projects/i3-video-wallpaper/setup.sh -p ~/视频/雪黎.mp4 -a -n -w -b -f max -g 64x64 > ~/log/vwp.out &
 
 # Bluetooth auto connection
-nohup bluetoothctl connect $HEADPHONE_MAC > ~/log/bluetooth.out
+nohup bluetoothctl connect $HEADPHONE_MAC > ~/log/bluetooth.out &
 
 # Proxy (Comment it since I am using transparent proxy)
 # nohup clash > ~/log/clash.out &
@@ -28,8 +28,8 @@ nohup bluetoothctl connect $HEADPHONE_MAC > ~/log/bluetooth.out
 # nohup bash "$dir/qq_notify_1.sh" > ~/log/qq_notify_1.out &
 # nohup bash "$dir/qq_notify_2.sh" > ~/log/qq_notify_2.out &
 
-# Redshift (I dont use it very often)
-# nohup redshift -l 23:113 > ~/log/redshift.out &
+# Redshift (Change color temperature)
+nohup redshift -t 6800:6800 -l 23:113 > ~/log/redshift.out &
 
 # Reset button state
 notify-send.py a --hint boolean:deadd-notification-center:true int:id:0 boolean:state:true type:string:buttons
